118f43f235556eccb5862a2fd0b9e32e811bd3bc
takfujii29 <57054773+takfujii29@users.noreply.github.com>
2020-06-05 17:48:41 +0900

    検索時に使用

---
+++ src/main/java/jp/co/aivick/demo/form/RecipeSearchForm.java
@@ -0,0 +1,15 @@
+package jp.co.aivick.demo.form;
+
+public class RecipeSearchForm {
+	
+	
+	private String search;
+
+	public String getSearch() {
+		return search;
+	}
+
+	public void setSearch(String search) {
+		this.search = search;
+	} 
+}
