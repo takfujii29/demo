0111be7bb219c4119b1f68934ae72bc619938481
takfujii29 <57054773+takfujii29@users.noreply.github.com>
2020-06-05 17:44:57 +0900

    immutableに変更

--- src/main/java/jp/co/aivick/demo/entity/Menu.java
+++ src/main/java/jp/co/aivick/demo/entity/Menu.java
@@ -7,50 +7,41 @@ import org.seasar.doma.GenerationType;
 import org.seasar.doma.Id;
 import org.seasar.doma.Table;
 
-@Entity
+@Entity(immutable = true)
 @Table(name = "menus")
 public class Menu {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "menu_id")
-	private Integer menuId;
+	final private Integer menuId;
 	
 	@Column(name = "menu_name")
-	private String menuName;
+	final private String menuName;
 	
 	@Column(name = "menu_type")
-	private String menuType;
+	final private String menuType;
 	
 	@Column(name = "price")
-	private Integer menuPrice;
+	final private Integer menuPrice;
 	
-	public void setMenuId(Integer menuId) {
+	public Menu(Integer menuId, String menuName, String menuType, Integer menuPrice) {
 		this.menuId = menuId;
+		this.menuName = menuName;
+		this.menuType = menuType;
+		this.menuPrice = menuPrice;
 	}
-	
+
 	public Integer getMenuId() {
 		return menuId;
-	}
-	
-	public void setMenuName(String menuName) {
-		this.menuName = menuName;
 	}
 	
 	public String getMenuName() {
 		return menuName;
 	}
 	
-	public void setMenuType(String menuType) {
-		this.menuType = menuType;
-	}
-	
 	public String getMenuType() {
 		return menuType;
-	}
-	
-	public void setMenuPrice(Integer menuPrice) {
-		this.menuPrice = menuPrice;
 	}
 	
 	public Integer getMenuPrice() {
