75fc6c814dca90aff5be75415a01927b8dbcd6ea
takfujii29 <57054773+takfujii29@users.noreply.github.com>
2020-06-05 17:49:12 +0900

    レシピの複数登録

--- src/main/java/jp/co/aivick/demo/service/MenuService.java
+++ src/main/java/jp/co/aivick/demo/service/MenuService.java
@@ -1,19 +1,26 @@
 package jp.co.aivick.demo.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+import org.seasar.doma.jdbc.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import jp.co.aivick.demo.dao.MenuDao;
+import jp.co.aivick.demo.dao.MenuRecipeDao;
 import jp.co.aivick.demo.entity.Menu;
+import jp.co.aivick.demo.entity.MenuRecipe;
 
 @Service
 public class MenuService {
 	
 	@Autowired
 	private MenuDao menuDao;
+	@Autowired
+	private MenuRecipeDao menuRecipeDao;
 	
 	public Menu findBy(String menuId) {
 		return menuDao.find(menuId);
@@ -24,9 +31,19 @@ public class MenuService {
 	}
 	
 	@Transactional
-	public Menu create(Menu menu) {
-		menuDao.insert(menu);
-		return menu;
+	public Menu create(Menu menu, String[] recipes) {
+		Result<Menu> menuResult = menuDao.insert(menu);
+		Menu createdMenu = menuResult.getEntity();
+		List<MenuRecipe> menuRecipe = IntStream.range(0, recipes.length)
+				                                 .mapToObj(i -> {
+				                                	 MenuRecipe m = new MenuRecipe();
+				                                	m.setRecipeId(Integer.parseInt(recipes[i]));
+				                                	 m.setMenuId(createdMenu.getMenuId());
+				                                	 return m;
+				                                 })
+				                                 .collect(Collectors.toList());
+		menuRecipeDao.insert(menuRecipe);
+		return createdMenu;
 	}
 //	
 
