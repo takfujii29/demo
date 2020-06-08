76576942b70aeecdc82af72a4a21a14af404d458
takfujii29 <57054773+takfujii29@users.noreply.github.com>
2020-06-08 13:46:05 +0900

    中間テーブル結合

--- src/main/resources/META-INF/jp/co/aivick/demo/dao/MenuDao/findAll.sql
+++ src/main/resources/META-INF/jp/co/aivick/demo/dao/MenuDao/findAll.sql
@@ -1,3 +1,9 @@
 select
-    /*%expand*/*
-from menus
+    M.menu_id,
+    M.menu_name,
+    M.menu_type,
+    M.price
+from menus M
+INNER JOIN menu_recipe MR on M.menu_id=MR.menu_id
+INNER JOIN recipes R ON MR.recipe_id=R.recipe_id
+group by M.menu_id;
